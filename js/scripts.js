4e35a29f1daf76a8486bca7a3b3c64d089bb1e1b
QIDOK <lyubimov.a.m.12@mail.ru>
2021-04-08 03:44:00 +0700

    08.04.2021

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,91 @@ MainModule.directive('levels', function() {
 		link: function(scope, element, attrs) {
 			element.append("<div class=\"next_level\"></div>");
 			$(".new_level").on("click", function() {
-				$("body").append("<div class=\"black\"></div>\
-					<div class=\"wincontent2\" id=\"winform\">\
+				$("body").append("\
+					<div class=\"black\"></div>\
+					<div class=\"wincontent2\">\
 						<div class=\"winheader\">\
-							<h2>Разблокировка</h2>\
+							<h2>Новый этап</h2>\
 						</div>\
-						<form id=\"form_unban\" method=\"POST\" ng-controller=\"popup\">\
-							<input type=\"text\" name=\"login\" placeholder=\"Ответственный\">\
-							<label>Время на выполнение</label>\
-							<input type=\"text\" ng-model=\"hours\" placeholder=\"чч\" required>\
-							{{hours}}\
-							<input type=\"text\" ng-model=\"minutes\" placeholder=\"мм\" required>\
-							<input type=\"submit\" value=\"Новый этап\">\
+						<form id=\"new_level_form\">\
+							<input type=\"text\" name=\"name\" placeholder=\"Ответственный\">\
+							<label id=\"time\">Время на выполнение:</label><br>\
+							<input type=\"timer\" name=\"hours\" placeholder=\"чч\" minlength=\"2\" maxlength=\"2\" >\
+							<label>:</label>\
+							<input type=\"timer\" name=\"minutes\" placeholder=\"мм\"minlength=\"2\" maxlength=\"2\" >\
+							<input type=\"submit\" value=\"Готово\">\
 						</form>\
-					</div>");
+					</div>\
+				");
 
 				$(".black").on("click", function() {
 					$(this).remove();
 					$(".wincontent2").remove();
+				});
+
+				$("#new_level_form").submit(function() {
+					var name = this.name.value;
+					var hours = this.hours.value;
+					var minutes = this.minutes.value;
+
+					$(this.name).css({"border":"2px solid"});
+					$(this.hours).css({"border":"2px solid"});
+					$(this.minutes).css({"border":"2px solid"});
+
+					$("#time").css({"color":"black", "font-size":"26px"})
+					$("#time").html("Время на выполнение:")
+
+					if(!name) {
+
+						$(this.name).css({"border":"2px solid red"});
+						$(this.name).attr("placeholder","Введите имя ответственного!");
+
+					} else if(!hours || !minutes) {
+
+						$(this.hours).css({"border":"2px solid red"});
+						$(this.minutes).css({"border":"2px solid red"});
+
+					} else if(hours[0] == '0' && hours[1] == '0' && minutes[0] == '0' && minutes[1] == '0') {
+
+						$(this.hours).css({"border":"2px solid red"});
+						$(this.minutes).css({"border":"2px solid red"});
+
+						$("#time").css({"color":"red", "font-size":"22px"})
+						$("#time").html("Время на выполнение:<br>Хотя бы одно число должно быть положительным!")
+
+					} else {
+
+
+
+					}
+
+					return false;
 				});
 			});
 		}
 	}
 });
 
-MainModule.controller('popup', function ($scope){
-	// if($scope.hours < 1 || $scope.hours > 99) {
-	// 	$("body").append("<div>Введите количество часов от 1 до 99</div>");
-	// }
-	$scope.hours = 14;
-});
+// MainModule.controller('popup', function ($scope){
+// 	$scope.hours = 12;
+// 	if(typeof($scope.hours) == 'number') {
+// 		$("body").append('<div>Удалось!</div>');
+// 	}
+// 	if(Number($scope.hours) > 99) {
+// 		$("body").append("<div>Удалось!</div>")
+// 	}
+// });
+
+// MainModule.directive('popup', function () {
+// 	return {
+// 		link: function (scope, element, attrs) {
+// 			console.log(element);
+// 			console.log(scope);
+// 			console.log(attrs);
+
+// 			if(element.hours > 99) {
+// 				$("body").append("<div>Удалось!</div>");
+// 			}
+// 		}
+// 	}
+// });
